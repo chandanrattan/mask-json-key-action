70dfb766994543c48926b917cea5c2450fadb4b4
yellowdraggon <chandan.rattan@gmail.com>
2024-05-23 17:44:50 +0530

    adding commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
-/**
- * The entrypoint for the action.
- */
-import { run } from './main'
+import * as core from '@actions/core';
+import * as fs from 'fs';
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-run()
+async function run(): Promise<void> {
+  try {
+    const jsonFilePath: string = core.getInput('json_file');
+    const tokenKey: string = core.getInput('token_key');
+
+    const rawData: string = fs.readFileSync(jsonFilePath, 'utf-8');
+    const jsonData: { [key: string]: any } = JSON.parse(rawData);
+
+    if (jsonData[tokenKey]) {
+      jsonData[tokenKey] = '****';
+    } else {
+      core.setFailed(`Key "${tokenKey}" not found in the JSON file`);
+      return;
+    }
+
+    const maskedJson: string = JSON.stringify(jsonData, null, 2);
+    console.log(maskedJson);
+
+    core.setOutput('masked_json', maskedJson);
+
+    // Optionally, save the masked JSON to a new file
+    const maskedJsonFilePath: string = jsonFilePath.replace('.json', '_masked.json');
+    fs.writeFileSync(maskedJsonFilePath, maskedJson);
+  } catch (error) {
+    core.setFailed((error as Error).message);
+  }
+}
+
+run();
